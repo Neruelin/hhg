bb1f02bae480bc5744c1aea5bbda33223c7a6833
Tyler <neruelin@gmail.com>
2018-11-05 18:02:12 -0500

    Update hhg.cpp

---
+++ hhg.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Pop
+{
+public:
+    Pop ()
+    {
+        map = {};
+    }
+    void set(int index, value)
+    {
+        map[index] = value;
+    }
+    int[] get()
+    {
+        return map;
+    }
+private:
+    int map [26];
+};
+
+class GA
+{
+public:
+
+    GA ()
+    {
+
+    }
+};
+
+
+
+int main() {
+
+
+
+
+
+    return 0;
+}
