90149cf05a630908dfb4809f816c17631bab4fe7
Tyler <neruelin@gmail.com>
2018-11-05 19:27:45 -0500

    Implemented Random Popgen
    RNG to create unique cipher keys for population class.

--- hhg.cpp
+++ hhg.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <ctime>
+
 using namespace std;
 
 class Pop
 {
 private:
-    int map [26] = {};
+    char map [26];
 public:
     Pop ()
     {
-    	
+        int i = 0;
+        for (i = 0; i < 26; i++) 
+        {
+            map[i] = 65 + (rand() % 26);
+            cout << map[i];
+        }	
+        cout << endl;
     }
     void set(int index, int value)
     {
         map[index] = value;
     }
-    int * get()
+    char * get()
     {
         return map;
     }
@@ -26,17 +36,25 @@ public:
 
     GA ()
     {
-
+       
     }
+    
+private:
+    vector<Pop> Population;
 };
 
 
-
 int main() {
-
-
-
-
-
+    //seeding rng with current time
+    srand(time(0));
+    /*
+    int a = 0;
+    while (a != 1) 
+    {
+          Pop();
+          cin >> a;
+    }
+    */
+    
     return 0;
 }
